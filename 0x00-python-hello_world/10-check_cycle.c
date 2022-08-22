83e547ef1c250a489201e26d56382d3194d129fd
Akinola <akinolatofunmi04@gmail.com>
2022-08-22 09:18:00 -0700

    First 10 task

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * check_cycle - check if a linked list contain a cycle
+ * @list: linked list to check
+ *
+ * Return: 1 if the link has a cycle
+ * Otherwise: 0
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list;
+	listint_t *fast = list;
+
+	if (!list)
+	{
+		return (0);
+	}
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
