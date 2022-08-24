92005ba430d162d8fae096bbb987b97f800f0308
Akinola <akinolatofunmi04@gmail.com>
2022-08-24 13:59:55 -0700

    Correction to equivalent

---
+++ 0x00-python-hello_world/101-main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+remove_char_at1 = __import__('101-remove_char_at_cpy').remove_char_at1
+
+print(remove_char_at1("Best School", 3))
+print(remove_char_at1("Chicago", 2))
+print(remove_char_at1("Python", -2))
+print(remove_char_at1("School", 10))
+print(remove_char_at1("C is fun!", 0))
