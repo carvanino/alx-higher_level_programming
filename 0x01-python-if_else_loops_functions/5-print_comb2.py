b471e23d38352dd5cb57b7ffcc0b2ca2cab66dd3
Akinola <akinolatofunmi04@gmail.com>
2022-08-23 21:27:23 -0700

    meet requirment

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for a in range(0, 10):
-    for b in range(0, 10):
-        if (a != 9 or b != 9):
-            print("{:d}{:d}" .format(a, b), end=", ")
-        else:
-            print("{:d}{:d}" .format(a, b))
+for a in range(100):
+    if a < 99:
+        print("{:02d}" .format(i), end=', ')
+    else:
+        print("99")
