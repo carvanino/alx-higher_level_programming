cf9aea3cf754804966091c7cc4e55a7dc1ecf8e6
Akinola <akinolatofunmi04@gmail.com>
2022-08-25 17:49:22 -0700

    checking fix

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,19 +7,21 @@ if __name__ == "__main__":
     if arg != 4:
         print("Usage: {} <a> <operator> <b>" .format(argv[0]))
         exit(1)
-    elif argv[2] == "+":
-        result = add(int(argv[1]), int(argv[3]))
+    a = int(argv[1])
+    b = int(argv[3])
+    operator = argv[2]
+
+    if operator == "+":
+        result = add(a, b)
         print("{} {} {} = {}" .format(argv[1], argv[2], argv[3], result))
-    elif argv[2] == "-":
-        result = sub(int(argv[1]), int(argv[3]))
+    elif operator == "-":
+        result = sub(a, b)
         print("{} {} {} = {}" .format(argv[1], argv[2], argv[3], result))
-        print(arg)
-    elif argv[2] == "*":
-        print(arg)
-        result = mul(int(argv[1]), int(argv[3]))
+    elif operator == "*":
+        result = mul(a, b)
         print("{} {} {} = {}" .format(argv[1], argv[2], argv[3], result))
-    elif argv[2] == "/":
-        result = div(int(argv[1]), int(argv[3]))
+    elif operator == "/":
+        result = div(a, b)
         print("{} {} {} = {}" .format(argv[1], argv[2], argv[3], result))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
