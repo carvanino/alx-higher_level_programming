9759c1c06fed7de2684cf520f94416cd788e42ca
Akinola <akinolatofunmi04@gmail.com>
2022-08-26 14:39:09 -0700

    Retrieves element from a list

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx < 0 or idx > len(my_list) - 1:
+        return None
+    else:
+        index = my_list[idx:idx+1]  #return (my-list[idx])
+        for i in index:
+            return(i)
