cab0106275e41de4568aa7a85750779e8e2e14ef
Akinola <akinolatofunmi04@gmail.com>
2022-08-28 13:45:41 -0700

    Return Tuples

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    length = len(sentence)
+    first = sentence[0:1]
+    if sentence  == "":
+        return (length, None)
+    return(length, first)
+
