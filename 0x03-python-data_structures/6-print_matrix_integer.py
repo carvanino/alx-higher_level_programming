35d67db085c0865c19ebe648128c461a7b8421e4
Akinola <akinolatofunmi04@gmail.com>
2022-08-28 15:02:24 -0700

    include format specifiers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for a in range(len(matrix)):
         for b in range(len(matrix[a])):
             if b != len(matrix) - 1:
-                print("{}" .format(matrix[a][b]), end=" ")
+                print("{:d}" .format(matrix[a][b]), end=" ")
             else:
-                print("{}" .format(matrix[a][b]), end="")
+                print("{:d}" .format(matrix[a][b]), end="")
         print()
