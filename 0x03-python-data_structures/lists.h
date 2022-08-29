d2a9ba386a00e4ad1eb4a2dfe0854d7bfc087b7c
Akinola <akinolatofunmi04@gmail.com>
2022-08-29 06:21:37 -0700

    include guard header file with standard files

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,11 +1,13 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct listint_s - single linked list
  * @n: integer
- * @next; points to the next node
+ * @next: points to the next node
  *
  * Description: singly linked list node structure for project
  */
