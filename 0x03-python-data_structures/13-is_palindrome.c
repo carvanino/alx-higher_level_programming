6fda7ac0dd14ef920f57cbe886929ed3eb769356
Akinola <akinolatofunmi04@gmail.com>
2022-08-29 06:14:50 -0700

    fix typo

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -32,13 +32,13 @@ listint_t *reverse_listint(lisint_t **head)
  * listint_len - returns the number of element in a loinked list
  *
  * @head: pointer to the first element of the list
- * 
+ *
  * Return: Number of element in the list
  */
 
 size_t listint_len(listint_t *head)
 {
-	size-t no_nodes;
+	size_t no_nodes;
 	listint_t *transv;
 
 	trans_v = head;
@@ -49,6 +49,7 @@ size_t listint_len(listint_t *head)
 		trans_v = trans_v->next;
 	}
 	return (no_nodes);
+}
 
 /**
  * is_palindrome - compared the linked list with its reversed if their equal
@@ -65,21 +66,15 @@ int is_palindrome(listint_t **head)
 	size_t nodes, i;
 
 	if (*head == NULL || *(head)->next == NULL)
-	{
 		return (1); /*An Empty list or one with a single node */
-	}
 	nodes = listint_len(*head); /* get the size of list */
 
 	transv = *head;
 	/* Get the middle of the list */
 	for (i = 0; i < (size / 2) - 1; i++)
-	{
 		transv = transv->next;
-	}
 	if ((size % 2) == 0 && transv->n != tmp->next->n)
-	{
 		return (0);
-	}
 	transv = transv->next->next;
 	rev_list = reverse_listint(&transv);
 	mid_list = rev_list;
@@ -88,13 +83,10 @@ int is_palindrome(listint_t **head)
 	while (rev_list != NULL)
 	{
 		if (ransv->n != rev_list->n)
-		{
 			return (0);
-		}
 		transv = transv->next;
 		rev_list = rev_list->next;
 	}
 	reverse_listint(&mid_list);
-
 	return (1);
 }
