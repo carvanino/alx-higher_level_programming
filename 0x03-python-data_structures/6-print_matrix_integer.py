7dbff3e7f09cceaef06376c96fd84659316183c8
Akinola <akinolatofunmi04@gmail.com>
2022-08-29 15:14:21 -0700

    ...

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,4 +5,4 @@ def print_matrix_integer(matrix=[[]]):
             print("{:d}" .format(matrix[a][b]), end="")
             if b != len(matrix) - 1:
                 print(" ", end="")
-        print()
+        print("")
