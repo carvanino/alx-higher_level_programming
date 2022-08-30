05286a753676274575759e576c6ee1be081233c0
Akinola <akinolatofunmi04@gmail.com>
2022-08-30 01:42:02 -0700

    Fixed problem from QA review

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for a in range(len(matrix)):
         for b in range(len(matrix[a])):
-            if b != len(matrix) - 1:
+            if b != len(matrix[a]) - 1:
                 print("{:d}" .format(matrix[a][b]), end=" ")
             else:
                 print("{:d}" .format(matrix[a][b]), end="")
