3b438d87e99d4809e2bff326ae1789df6fe84fd5
Akinola <akinolatofunmi04@gmail.com>
2022-08-31 20:43:56 -0700

    checker fix

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
+    return len(a_dictionary.keys())
+"""
     no_keys = 0
-    if not a_dictionary:
-        return
     for key, a in enumerate(a_dictionary):
         no_keys = no_keys + key
     return no_keys
+"""
