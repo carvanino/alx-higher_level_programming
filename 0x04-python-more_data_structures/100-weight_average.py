43c0ac4341a96542ef5aff9ffa2be4cba378944c
Akinola <akinolatofunmi04@gmail.com>
2022-08-31 20:32:54 -0700

    Working with tuples to get the weighted average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    sum = 0
+    multiples = 0
+    if not my_list:
+        return 0
+    for element in my_list:
+        sum = sum + element[1]
+        multiples = multiples + (element[0] * element[1])
+        weightd_avg = multiples / sum
+    return float(weightd_avg)
+        
