83deb73e1cbd14785cb68b8cc8b0b1871162cc31
Akinola <akinolatofunmi04@gmail.com>
2022-08-31 20:26:27 -0700

    Include not condtion

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
     no_keys = 0
+    if not a_dictionary:
+        return
     for key, a in enumerate(a_dictionary):
         no_keys = no_keys + key
     return no_keys
