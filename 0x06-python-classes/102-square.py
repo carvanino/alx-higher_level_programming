27349b47d2bea9848b362a4ccea753bb9c12b3b0
Akinola <akinolatofunmi04@gmail.com>
2022-09-13 19:32:05 -0700

    Compare 2 square

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+""" Access and update private attribute"""
+
+
+class Square:
+    """ Defines a class 'Square' """
+    def __init__(self, size=0):
+        """ Initializes class square and raise exceptions
+        when size is not an int or is negative
+        Args:
+            size: size of square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """ Retrieves the private attribute (size)"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ Sets the size/property of the class to value """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """ Gets the area of the square"""
+        return self.__size ** 2
+
+    def __eq__(self, other):
+        """ Define the == comparision to a Square"""
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        """ Define the != compariosn to a Square """
+        return self.area() != other.area()
+
+    def __lt__(self, other):
+        """ Define the < comaprison to a Square"""
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        """ Defines the <= comaprison for a Square """
+        return self.area() <= other.area()
+
+    def __gt__(self, other):
+        """ Defines the > comaprison to a Square """
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        """ Define the >= comparison to a Square """
+        return self.area() >= other.area()
