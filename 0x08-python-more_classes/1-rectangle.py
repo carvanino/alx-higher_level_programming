3cd844c7dafec91d36c1d2c39bd3bcefd96afd81
Akinola <akinolatofunmi04@gmail.com>
2022-09-19 12:36:28 -0700

    ...

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -20,8 +20,8 @@ class Rectangle:
             TypeError: width or height must be an integer
             ValueError: width or height must be >= 0
        """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
         @property
         def width(self):
