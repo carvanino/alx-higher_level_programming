edecdf962ab2f281b84fce8244440b593432bbe0
Akinola <akinolatofunmi04@gmail.com>
2022-09-30 21:43:15 +0100

    Task 2 - Files addes but not tested

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+""" rectangle Module
+Houses a subclass Rectangle
+"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """ Defines the Rectangle subclass
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None) -> None:
+        super().__init__(id, id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """Retrieves the private attribute (width)"""
+        return self.__width
+    @width.setter
+    def width(self, value):
+        """Set the width of the class to value """
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrives the private attribute (height) """
+        return self.__height
+    @height.setter
+    def height(self, value):
+        """Sets the (height) as a private attribute """
+        self.__height = value
+    
+    @property
+    def x(self):
+        """ Retrieves the private attribute (x) """
+        return self.__x
+    @x.setter
+    def x(self, value):
+        """Sets (x) as a private attribute """
+        self.__x = value
+    
+    @property
+    def y(self):
+        """ Retrieves the private attribute (y) """
+        return self.__y
+    def y(self, value):
+        """Sets (y) as a private attribute """
+        self.__y = value
