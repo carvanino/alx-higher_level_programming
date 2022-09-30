88c36f20a797e6fdee623e444e1e60e205da6679
Akinola <akinolatofunmi04@gmail.com>
2022-09-30 13:46:17 -0700

    Task 2 - First Rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -10,7 +10,7 @@ class Rectangle(Base):
     """
 
     def __init__(self, width, height, x=0, y=0, id=None) -> None:
-        super().__init__(id, id)
+        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
