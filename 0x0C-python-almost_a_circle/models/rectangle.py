02b330fb05a73694319045a9c6e7721435c8f2a7
Akinola <akinolatofunmi04@gmail.com>
2022-09-30 15:33:30 -0700

    Display #1

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -83,10 +83,14 @@ class Rectangle(Base):
         instance to stdout with the character '#'
         """
 
+        print("\n" * self.__y, end='')
         for y in range(self.__height):
+            print(" " * self.__x, end='')
             print("#" * self.__width)
 
     def __str__(self):
         """ Prints an informal representaion of an instance Rectangle
         """
         return ("[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height))
+
+
