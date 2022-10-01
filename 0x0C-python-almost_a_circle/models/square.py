67681d234349f88639b9ae90390857b854d279ed
Akinola <akinolatofunmi04@gmail.com>
2022-10-01 13:11:22 -0700

    Task 11 - Square size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -17,3 +17,17 @@ class Square(Rectangle):
         """
         return ("[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width))
 
+    @property
+    def size(self):
+        """ Retrieves the size of square
+        """
+
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """ Sets the size of square to value
+        """
+
+        self.width = value
+        self.height = value
