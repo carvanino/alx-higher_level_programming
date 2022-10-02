c19e0b01a55270ec46801396a10cc2f111fb9f17
Akinola <akinolatofunmi04@gmail.com>
2022-10-02 04:39:16 -0700

    Task 16 - JSONstring to file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,7 +18,7 @@ class Base:
             id: (int) Defaults as None
 
         """
-        if id != None:
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
@@ -34,3 +34,23 @@ class Base:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ Writes the JSON string representation of list_objs
+        to a file
+        """
+
+        filename = '{}.json'.format(cls.__name__)
+        print(filename)
+        list_dict = []
+        if list_objs is None:
+            with open(filename, mode='w') as f:
+                f.write(list_dict)
+        else:
+            for i, obj in enumerate(list_objs):
+                print(i)
+                list_dict.append(obj.to_dictionary())
+            json_string = cls.to_json_string(list_dict)
+            with open(filename, mode='w') as f:
+                f.write(json_string)
