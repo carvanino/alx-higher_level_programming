8b81247869a40d0752db65316117c390ab436109
Akinola <akinolatofunmi04@gmail.com>
2022-10-03 20:55:31 -0700

    fix empty list as string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -47,14 +47,14 @@ class Base:
         """
 
         filename = '{}.json'.format(cls.__name__)
-        #print(filename)
-        list_dict = []  # a list of object dictionaries
+        #  print(filename)
+        list_dict = []  #  a list of object dictionaries
         if list_objs is None or len(list_objs) == 0:
             with open(filename, mode='w') as f:
-                f.write(list_dict)
+                f.write("[]")
         else:
             for i, obj in enumerate(list_objs):
-                #print(i)
+                #  print(i)
                 list_dict.append(obj.to_dictionary())
             json_string = cls.to_json_string(list_dict)
             with open(filename, mode='w') as f:
