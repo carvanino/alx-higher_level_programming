abc55a9f44c7cebe84639d1e706a422991cb2e1a
Akinola <akinolatofunmi04@gmail.com>
2022-10-03 20:27:22 -0700

    typo

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -24,5 +24,5 @@ class Base:
         if id is not None:
             self.id = id
         else:
-            BAse.__nb_objects += 1
+            Base.__nb_objects += 1
             self.id = BAse.__nb_objects
