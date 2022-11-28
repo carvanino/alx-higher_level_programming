c65dcd3d23b5954c172425d8605afaddbe088627
Akinola <akinolatofunmi04@gmail.com>
2022-11-28 13:10:49 -0800

    Task 12 - Object

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const args = process.argv;
+const length = args.length;
+let temp;
+
+if (length <= 3) {
+  console.log(0);
+} else {
+  for (let i = 2; i < length; i++) {
+    for (let j = i + 1; j < length - 1; j++) {
+      if (args[j] > args[i]) {
+        temp = args[j];
+        args[j] = args[i];
+        args[i] = temp;
+      }
+    }
+  }
+}
+console.log(args[3]);
