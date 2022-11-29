a38152f266aa8a302dee5373594f64fb6e1f6548
Akinola <akinolatofunmi04@gmail.com>
2022-11-28 16:55:47 -0800

    Finding the one that works

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,26 +1,19 @@
 #!/usr/bin/node
-
 const args = process.argv;
 const length = args.length;
 let temp;
-
 if (length <= 3) {
   console.log(0);
 } else {
-  for (let i = 2; args[i]; i++) {
-    for (let j = i + 1; args[j]; j++) {
-      if (args[j] >= args[i]) {
+  for (let i = 2; i < length; i++) {
+    for (let j = i + 1; j < length; j++) {
+      if (args[j] > args[i]) {
         temp = args[j];
         args[j] = args[i];
         args[i] = temp;
-        /* console.log(args[i]); */
-        /* console.log(args[j]); */
       }
     }
-	  /*console.log('i is:', i);*/
   }
-  /*for (let i = 2; i < length; i++) {
-    console.log(args[i]);
-  }*/
-  console.log(args[length - 2]);
+  console.log(args[3]);
 }
+/* console.log(args[3]); */
