5d4fe787d8404ed6f033cde06adaec67496496fe
Akinola <akinolatofunmi04@gmail.com>
2022-12-02 15:27:58 -0800

    Does this work

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -15,12 +15,11 @@ if __name__ == '__main__':
             db=sys.argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states \
-            WHERE states.name LIKE 'N%' \
+    cur.execute("SELECT * FROM states WHERE states.name LIKE 'N%' \
             ORDER BY states.id ASC;")
 
     states = cur.fetchall()
     for state in states:
         print(state)
     cur.close()
-    db.close()
+    # db.close()
