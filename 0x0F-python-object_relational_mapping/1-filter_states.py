5baee60556e8c22c26732a6190d20205fe326a4f
Akinola <akinolatofunmi04@gmail.com>
2022-12-02 15:03:30 -0800

    Fix pycodestyle

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,14 +13,14 @@ if __name__ == '__main__':
             user=sys.argv[1],
             passwd=sys.argv[2],
             db=sys.argv[3])
+
     cur = db.cursor()
-    cur.execute("SELECT * FROM states\
-            WHERE upper(states.name) LIKE 'N%'\
-            ORDER BY states.id ASC")
-    
+    cur.execute("SELECT * FROM states \
+            WHERE upper(states.name) LIKE 'N%' \
+            ORDER BY states.id ASC;")
+
     states = cur.fetchall()
     for state in states:
         print(state)
     cur.close()
     db.close()
-
