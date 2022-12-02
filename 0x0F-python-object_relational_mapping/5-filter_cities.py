5591a03c3638e277dfb2442db4872c5a81453062
Akinola <akinolatofunmi04@gmail.com>
2022-12-02 15:21:14 -0800

    Fix pycodestyle

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
             ON cities.state_id = states.id\
             WHERE states.name = %s \
             ORDER BY cities.id ASC;", (sys.argv[4], ))
-    
+
     cities = cur.fetchall()
 
     for i, city in enumerate(cities):
