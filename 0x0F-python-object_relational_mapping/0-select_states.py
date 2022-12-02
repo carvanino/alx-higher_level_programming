22fdd50aff973e036ca4ee64273e091657f7a6f3
Akinola <akinolatofunmi04@gmail.com>
2022-12-02 14:36:02 -0800

    Task Debug checker fail

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
             host='localhost',
             user=sys.argv[1],
             passwd=sys.argv[2],
-            db='hbtn_0e_0_usa',
+            db=sys.argv[3],
             port=3306)
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY states.id ASC")
