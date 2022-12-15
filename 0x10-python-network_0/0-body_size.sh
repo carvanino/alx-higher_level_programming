3ba42e16a2680896794beb04fbc56e9d223c62e7
Akinola <akinolatofunmi04@gmail.com>
2022-12-15 13:47:49 -0800

    Really

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 # Takes in a URL, sends a request to a URL and displayes the size of the body of the respose
-curl -s -I "$@" | grep 'Content-Length' | cut -d ' ' -f2
+curl -sI "$@" | grep 'Content-Length' | cut -d ' ' -f2
