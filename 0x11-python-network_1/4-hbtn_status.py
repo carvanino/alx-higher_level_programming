89aaa88c192ae112b8d54c3d1115574e68599d15
Akinola <akinolatofunmi04@gmail.com>
2022-12-16 06:10:34 -0800

    Task 4 - What's my status #1

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+A script that fetches https://alx-intranet.hbtn.io/status using request package
+"""
+
+import requests
+
+if __name__ == '__main__':
+    r = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t - type: {}" .format(type(r.content.decode('utf-8'))))
+    print("\t - content: {}" .format(r.content.decode('utf-8')))
