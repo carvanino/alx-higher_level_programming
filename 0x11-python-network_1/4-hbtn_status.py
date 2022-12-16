60db8be95aabce254a0f44f832de27909460607a
Akinola <akinolatofunmi04@gmail.com>
2022-12-16 06:14:02 -0800

    Output fail

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,7 +5,7 @@ A script that fetches https://alx-intranet.hbtn.io/status using request package
 
 import requests
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     r = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:")
     print("\t - type: {}" .format(type(r.content.decode('utf-8'))))
