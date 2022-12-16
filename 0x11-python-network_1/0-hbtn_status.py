e0275b5fbf5c54d7b4f507e5fbecaa8cfd216ee7
Akinola <akinolatofunmi04@gmail.com>
2022-12-16 05:24:25 -0800

    My fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-""" A module that fetches from a url """
-from urllib import request
+""" A module that fetches https://intranet.hbtn.io/status using urllib"""
+import urllib.request
 
 if __name__ == "__main__":
-    page = request.Request("https://intranet.hbtn.io/status")
-    with request.urlopen(page) as fetch:
-        content = fetch.read()
+    req = urllib.request.Request("https://intranet.hbtn.io/status")
+    with urllib.request.urlopen(req) as fetch:
+        html = fetch.read()
         print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode("utf-8")))
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode("utf-8")))
