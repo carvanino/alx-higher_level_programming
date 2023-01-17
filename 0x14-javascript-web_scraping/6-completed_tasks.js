33ad523ee1fc03f79899b8ea10a460f3a3d4dbad
Akinola <akinolatofunmi04@gmail.com>
2023-01-17 19:35:22 +0000

    Fix semistandard

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -4,20 +4,17 @@ const request = require('request');
 
 request.get(args[2], function (error, response, body) {
   if (!error) {
-    let count = 0;
-    let id = 1;
     const userTask = {};
     const data = JSON.parse(body);
     for (const task of data) {
-	if (task.completed) {
-		if (task.userId in userTask) {
-			userTask[task.userId]++;
-		}
-		else {
-			userTask[task.userId] = 1
-		}
-	}
+      if (task.completed) {
+        if (task.userId in userTask) {
+          userTask[task.userId]++;
+        } else {
+          userTask[task.userId] = 1;
+        }
       }
+    }
     console.log(userTask);
   }
   // console.log(userTask);
